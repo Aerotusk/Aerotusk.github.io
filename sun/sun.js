64897b876770d0a213f603431b54748259024a69
Daniel Maas <danny.maas@keysight.com>
2025-05-19 13:53:37 -0700

    Moved the Interloper to its own orbit. Made the sun grow a bit on Hover (see todo). Purged some old tasks which have since been completed

--- sun/sun.js
+++ sun/sun.js
@@ -1,19 +1,19 @@
 const refDate = new Date("2025-01-01")
-var planets = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune","pluto","interloper"]
-var orbitPeriods = [88.0,224.7,365.2,687.0,4331,10747,30589,59800,90560,1/24] //orbital period in days
+var planets = ["mercury","venus","earth","mars","interloper","jupiter","saturn","uranus","neptune","pluto"]
+var orbitPeriods = [88.0,224.7,365.2,687.0,1/24,4331,10747,30589,59800,90560] //orbital period in days
 // var orbitPeriods = [0.1/36000,0.15/36000,0.2/36000,0.25/36000,0.3/36000,0.35/36000,0.4/36000,0.45/36000,0.5/36000,0.55/36000] // For debugging purposes
 var orbitPositions = [
     48.30+29.2+125.0,
     76.6+51.5+279.3,
     190.2+273.0+357.3,
     49.48+286.71+132.6,
+    0,
     100.5+273.6+63.9,
     113.6+337.2+258.7,
     74.0+90.5+250.9,
     131.9+268.1+318.7,
-    110.2+113.6+78.0,
-    0]
-var orbitRadius = [50,100,150,200,250,300,350,400,450,300]
+    110.2+113.6+78.0]
+var orbitRadius = [50,100,150,200,250,300,350,400,450,500]
 
 planetMovement();
 
@@ -34,7 +34,7 @@ function planetMovement()
             var height = parseInt(style.getPropertyValue('height'),10);
             let timeDiff = (date.getTime() - refDate.getTime());
             // timeDiff=0;
-            let orbitTime = 360 * ((timeDiff) / 86400000) / orbitPeriods[i]; //Number of degrees since Jan 1 2025 (modulo)
+            let orbitTime = 360 * ((timeDiff) / 86400000) / orbitPeriods[i];
             let xCoord = orbitRadius[i] * Math.cos((orbitPositions[i] - orbitPositions[2] + 90 + orbitTime) * Math.PI / 180) * 1.5;
             let yCoord = -orbitRadius[i] * Math.sin((orbitPositions[i] - orbitPositions[2] + 90 + orbitTime) * Math.PI / 180) / 2;
             xCoord -= width / 2;
