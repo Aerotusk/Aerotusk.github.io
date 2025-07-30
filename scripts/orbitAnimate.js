fb3eebea33fdd49a6614209dd4cc9d14959ca39c
Aerotusk <odannyboy001@gmail.com>
2025-07-29 17:04:31 -0700

    Hooooly smokes. Made planet animation code more modular. Deployed it across all the necessary pages. Made navbar populate via script for easier editing and deployment. Renamed "s" folder to "css"

---
+++ scripts/orbitAnimate.js
@@ -0,0 +1,36 @@
+//This file should ONLY include functions for animating orbits
+
+const firstPtAries = 190.2+273.0+357.3;
+
+function calculateOrbits(planetList)
+{
+    let id = null;
+    clearInterval(id);
+    id = setInterval(animatePlanet,30);
+    function animatePlanet()
+    {
+        var date = new Date();
+        let timeDiff = (date.getTime() - refDate.getTime());
+        
+        // Set locations of planets
+        for (i =1; i < planetList.length; i++)
+        {   
+            var planet = planetList[i]
+            var obj = planet.pName;
+            var element = document.getElementById(obj);
+            var style = window.getComputedStyle(element);
+            var size = parseFloat(style.getPropertyValue('width'),10); //width and height are equal for all planets
+            let orbitTime = 360 * ((timeDiff) / 86400000) / planet.per;
+
+            let xCoord = -planet.rad * Math.sin((planet.pos - firstPtAries + orbitTime) * Math.PI / 180) * 1.5; //Make it kinda elliptical
+            let yCoord = -planet.rad * Math.cos((planet.pos - firstPtAries + orbitTime) * Math.PI / 180) / 2;
+
+            planet.org = [xCoord,yCoord];
+            xCoord -= size / 2;
+            yCoord -= size / 2;
+
+            document.getElementById(obj).style.transform = 'translate('+ xCoord + 'px, ' + yCoord + 'px)';
+            document.getElementById(obj).style.zIndex = Math.round(yCoord+1000); // Sets draw order of planets
+        }
+    }
+}
