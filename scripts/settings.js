054e87c93dd296827183a1a2931594e13da31c37
Aerotusk <odannyboy001@gmail.com>
2026-01-26 14:18:47 -0800

    Made settings icon grow and shrink. Found out how to use css cursors property!

---
+++ scripts/settings.js
@@ -0,0 +1,59 @@
+// This file should ONLY include items relevant planetResize and helper functions
+
+const sAnimationDuration = 80;
+const sAnimationShift = 74;
+const sAnimationRate = 23;
+const sResizeAmount = 0.1;
+
+function settingsPage(event)
+{
+    console.log("you were expecting the settings page, weren't you?")
+    
+}
+
+function settingsResize(event)
+{
+    let lmnt = document.getElementById(event.id);
+    
+    let anId = null;
+    clearInterval(anId);
+    anId = setInterval(stAnimate,15); // Frame period, in ms
+    let tempStep = 0;
+    let step = 1;
+    let dim = 48;
+    let xOrg = -12;
+    let yOrg = 12;
+    function stAnimate()
+    {
+        if(tempStep != step) // If the animation is still playing:
+        {
+            let size = dim * (1 + sResizeAmount * (stLogistic(step + sAnimationShift,sAnimationRate,sAnimationDuration)));
+
+            let xCoord = xOrg + ((size - dim)/2);
+            let yCoord = yOrg - ((size - dim)/2);
+
+            lmnt.style.width = size + 'px';
+            lmnt.style.height = size + 'px';
+            lmnt.style.transform = 'translate('+ xCoord + 'px, ' + yCoord + 'px)';
+            tempStep = step;
+        }
+
+        if(lmnt.matches(':hover')){ // If mouse is still over the planet:
+            if(step < (sAnimationDuration * 2) - sAnimationShift - 55) {
+                step++;
+            }
+        } else {
+            if(step > 0) {
+                step--;
+            } else {
+                clearInterval(anId);
+            }
+        }
+    }
+}
+
+// HELPER FUNCTIONS
+function stLogistic(time,rate,dur)
+{
+    return 1 / (1 + Math.exp(-rate * ((time/dur)-1)))
+}
